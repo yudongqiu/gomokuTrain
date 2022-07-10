588f06074501c0610f84e3f5da04c7bc36ae912f
qyd <saviorbruce@gmail.com>
2022-07-10 13:46:51 -0700

    small clean up

--- ai_server/launch_server.py
+++ ai_server/launch_server.py
@@ -2,6 +2,4 @@
 from server import app, socketio
 
 if __name__ == "__main__":
-    #app.run(debug=True, use_reloader=False, port=5005)
-    #socketio.run(app, debug=True, use_reloader=False, port=5005)
     socketio.run(app, debug=False, use_reloader=False, port=5005)
